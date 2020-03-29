3de4ae96955405c1768bb2db78255ba99eaccd38
Euclidean13 <aacristellys@icloud.com>
2020-03-29 20:15:26 +0200

    master: added resttemaplate and feign clients to call endpoints between microservicies v2

--- springboot-servicio-item/src/main/java/com/app/item/springbootservicioitem/clientes/ProductoClientRest.java
+++ springboot-servicio-item/src/main/java/com/app/item/springbootservicioitem/clientes/ProductoClientRest.java
@@ -1,4 +1,19 @@
 package com.app.item.springbootservicioitem.clientes;
 
+import com.app.item.springbootservicioitem.models.Producto;
+import org.springframework.cloud.openfeign.FeignClient;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.List;
+
+
+@FeignClient(name = "servicio-productos", url="localhost:8001")
 public interface ProductoClientRest {
+
+    @GetMapping("/listar")
+    public List<Producto> listar();
+
+    @GetMapping("/ver/{id}")
+    public Producto detalle(@PathVariable Long id);
 }
--- springboot-servicio-item/src/main/java/com/app/item/springbootservicioitem/models/service/ItemServiceFeign.java
+++ springboot-servicio-item/src/main/java/com/app/item/springbootservicioitem/models/service/ItemServiceFeign.java
@@ -1,4 +1,26 @@
 package com.app.item.springbootservicioitem.models.service;
 
-public class ItemServiceFeign {
+import com.app.item.springbootservicioitem.clientes.ProductoClientRest;
+import com.app.item.springbootservicioitem.models.Item;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+@Service("serviceFeign")
+public class ItemServiceFeign implements ItemService{
+
+    @Autowired
+    private ProductoClientRest clienteFeign;
+
+    @Override
+    public List<Item> findAll() {
+        return clienteFeign.listar().stream().map(p -> new Item(p, 1)).collect(Collectors.toList());
+    }
+
+    @Override
+    public Item findById(Long id, Integer cantidad) {
+        return new Item(clienteFeign.detalle(id), cantidad);
+    }
 }
