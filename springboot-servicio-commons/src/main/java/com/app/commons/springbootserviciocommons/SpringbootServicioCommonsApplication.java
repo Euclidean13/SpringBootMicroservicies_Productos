be7cca2b08b5d732b3de569105112c08224f2d9c
Euclidean13 <aacristellys@icloud.com>
2020-04-05 21:57:25 +0200

    master: adds commons project for sharing models, POJOs...

---
+++ springboot-servicio-commons/src/main/java/com/app/commons/springbootserviciocommons/SpringbootServicioCommonsApplication.java
@@ -0,0 +1,8 @@
+package com.app.commons.springbootserviciocommons;
+
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootServicioCommonsApplication {
+
+}
