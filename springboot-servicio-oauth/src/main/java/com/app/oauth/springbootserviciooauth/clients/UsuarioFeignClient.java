ee57bd38cf2e75914eca9e2bf3ee30767334e3ee
Euclidean13 <aacristellys@icloud.com>
2020-04-11 01:51:30 +0200

    master: Implements events. Three login tries

--- springboot-servicio-oauth/src/main/java/com/app/oauth/springbootserviciooauth/clients/UsuarioFeignClient.java
+++ springboot-servicio-oauth/src/main/java/com/app/oauth/springbootserviciooauth/clients/UsuarioFeignClient.java
@@ -2,8 +2,7 @@ package com.app.oauth.springbootserviciooauth.clients;
 
 import com.app.usuarios.commons.springbootserviciousuarioscommons.models.entity.Usuario;
 import org.springframework.cloud.openfeign.FeignClient;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.*;
 
 @FeignClient(name = "servicio-usuarios")
 public interface UsuarioFeignClient {
@@ -11,4 +10,6 @@ public interface UsuarioFeignClient {
     @GetMapping("/usuarios/search/buscar-username")
     public Usuario findByUsername(@RequestParam(name = "username") String username);
 
+    @PutMapping("/usuarios/{id}")
+    public Usuario update(@RequestBody Usuario usuario, @PathVariable Long id);
 }
--- springboot-servicio-oauth/src/main/java/com/app/oauth/springbootserviciooauth/services/IUsuarioService.java
+++ springboot-servicio-oauth/src/main/java/com/app/oauth/springbootserviciooauth/services/IUsuarioService.java
@@ -1,7 +1,10 @@
 package com.app.oauth.springbootserviciooauth.services;
 
 import com.app.usuarios.commons.springbootserviciousuarioscommons.models.entity.Usuario;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestBody;
 
 public interface IUsuarioService {
     public Usuario findByUsername(String username);
+    public Usuario update(Usuario usuario, Long id);
 }
