43d1b37b853d7b5c055e15d52c6e9bc10c79dfbf
Euclidean13 <aacristellys@icloud.com>
2020-04-12 13:41:48 +0200

    master: Adds RabbitMQ setup and configuration

--- springboot-servicio-usuarios/src/main/java/com/app/usuarios/springbootserviciousuarios/SpringbootServicioUsuariosApplication.java
+++ springboot-servicio-usuarios/src/main/java/com/app/usuarios/springbootserviciousuarios/SpringbootServicioUsuariosApplication.java
@@ -1,8 +1,13 @@
 package com.app.usuarios.springbootserviciousuarios;
 
+import org.springframework.amqp.core.AmqpTemplate;
+import org.springframework.amqp.core.Queue;
+import org.springframework.amqp.rabbit.annotation.RabbitListener;
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.Bean;
 
 @EntityScan({"com.app.usuarios.commons.springbootserviciousuarioscommons.models.entity"})
 @SpringBootApplication
@@ -12,4 +17,18 @@ public class SpringbootServicioUsuariosApplication {
 		SpringApplication.run(SpringbootServicioUsuariosApplication.class, args);
 	}
 
+	@Bean
+	public ApplicationRunner runner(AmqpTemplate template) {
+		return args -> template.convertAndSend("myqueue", "foo");
+	}
+
+	@Bean
+	public Queue myQueue() {
+		return new Queue("myqueue");
+	}
+
+	@RabbitListener(queues = "myqueue")
+	public void listen(String in) {
+		System.out.println(in);
+	}
 }
---
+++ zipkin.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export RABBIT_ADDRESSES=localhost:5672
+java -jar /Users/aac/Workspace/Tools/Monitoring/Zipkin/zipkin-server-2.21.0-exec.jar
